a076f35a2b4370bdf79ba0ccc7ea534c899f2e52
Pranjal Singh <008pranjalsingh@gmail.com>
2019-10-12 13:42:25 +0530

    Create set1.1.py

---
+++ cryptography/set1.1.py
@@ -0,0 +1,12 @@
+def hextobase64():
+    a='49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    b = a.decode("hex")
+    c = b.encode("base64") 
+   
+    print(c)  
+    
+        
+             
+
+if __name__=="__main__":
+    hextobase64()
