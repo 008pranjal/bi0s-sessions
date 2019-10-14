2e76b903f6f591c1019b4025fdc768d3825b5f23
Pranjal Singh <008pranjalsingh@gmail.com>
2019-10-14 18:04:15 +0530

    Create twin.py

---
+++ forensics/twin.py
@@ -0,0 +1,11 @@
+with open('./Twin1', 'rb') as f:
+    twin1 = f.read()
+   
+with open('./Twin2', 'rb') as f:
+    twin2 = f.read()
+   
+f = ''
+for i in range(min(len(twin1), len(twin2))):
+    if twin1[i] != twin2[i]:
+        flag += str(twin2[i])
+print(f)
